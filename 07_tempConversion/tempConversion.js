fa8050b9fe90dcadfd49f71902b9549f0d6a70d3
Kristos-Resurreccion <101231670+Kristos-Resurreccion@users.noreply.github.com>
2024-07-01 16:12:59 +1000

    Complete Odin Javascript Practice

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (inputF) {
+  let rawTempF = (inputF - 32) * .55555555555;
+  let roundedTempF = Math.round(rawTempF * 10) / 10;
+
+  return roundedTempF;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (inputC) {
+  let rawTempC = ((inputC * 1.8) + 32);
+  let roundedTempC = Math.round(rawTempC * 10) / 10;
+
+  return roundedTempC;
 };
 
 // Do not edit below this line
